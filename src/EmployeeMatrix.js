194eca81f1df6cccec490f988b935714d418b23c
Nadavgo8 <19nadav99@gmail.com>
2025-07-15 00:45:55 +0300

    completed ex3

--- src/EmployeeMatrix.js
+++ src/EmployeeMatrix.js
@@ -1,12 +1,38 @@
-/* Write your code below */
+const Matrix = require("./Matrix"); // Adjust the path if needed
 
+class EmployeeMatrix extends Matrix {
+  constructor(numRows, numColumns) {
+    super(numRows, numColumns); // call parent constructor
+  }
 
+  loadData(salaryData) {
+    this.matrix = salaryData.map((obj) => Object.values(obj));
+  }
 
+  // You can now add methods specific to employees
+}
 
-//You can paste the code from the lesson below to test your solution
+let data = [
+  { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
+  { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
+  { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
+  { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
+  { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
+  { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 },
+];
 
+let m = new EmployeeMatrix();
 
+m.loadData(data);
+m.print();
+//prints
 
+// e10021  Gillian Finance 2000
+// e10725  Tibor   Design  1200
+// e10041  Anisha  Finance 2300
+// e10411  Jakub   Design  1600
+// e11995  Mar     Design  1300
+// e10732  Nisha   Design  1200
 
 /* Do not remove the exports below */
-module.exports = EmployeeMatrix
+module.exports = EmployeeMatrix;
